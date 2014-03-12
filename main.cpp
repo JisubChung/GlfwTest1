59bbeaea677faa1893387ea18d89e05e791c7ed3
Ji-sub Chung <jisub.chung0@gmail.com>
2014-03-11 23:09:55 -0700

    Put a window up
    Initial wasn't able to keep a window open due to processing. Fixed by
    adding glfwPollEvents();

--- main.cpp
+++ main.cpp
@@ -2,23 +2,50 @@
 //
 
 #include "stdafx.h"
-#define GLFW_INCLUDE_GLU
+#include <stdlib.h>
+#include <stdio.h>
 #include <GLFW\glfw3.h>
+
+static void error_callback(int error, const char* description) 
+{
+    fputs(description, stderr);
+}
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
 
 int main()
 {
-	glfwInit();
-	GLFWwindow * window = glfwCreateWindow(640, 480, "My first window", NULL, NULL);
-	if (!window)
+	GLFWwindow * window; //creates GLFW window
+	glfwSetErrorCallback(error_callback); //enables error report
+	
+	glfwInit(); //initializes glfw library
+	if (!glfwInit()) //makes sure initialization has happened
+	{
+		exit(EXIT_FAILURE);
+	}
+	window = glfwCreateWindow(640, 480, "My first window", NULL, NULL); //makes the window into a glfw window object
+	if (!window) //checks for window creation
 	{
 		glfwTerminate();
-		return (0);
+		exit(EXIT_FAILURE);
 	}
 
-	glfwMakeContextCurrent(window);
+	glfwMakeContextCurrent(window); //makes the OpenGL context current (?)
+	glfwSetKeyCallback(window, key_callback); //setting callback to close window
+	while (!glfwWindowShouldClose(window)) //keeps window open
+	{
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+		glViewport(0,0,width,height);
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
-	return 0;
+		glfwSwapBuffers(window);
+		//glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window); //frees memory taken by the window
+	glfwTerminate(); //removes glfw library usage
+	exit (EXIT_SUCCESS);
 }
-
